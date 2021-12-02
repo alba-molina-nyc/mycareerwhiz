59e1bd9e5008644f0aa485e4de1f7fa833b1628b
alba-molina-nyc <avmanyc@gmail.com>
2021-12-02 14:14:00 -0500

    home dot html loops through the app object and lists all the items from the object

--- theapplicationswhiz/urls.py
+++ theapplicationswhiz/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
 from . import views
 
 
 urlpatterns = [
-    # we will define all app-level urls in this list
-    path('', views.home, name='home'),
+   
+    path('', views.HomeView.as_view(), name='home'),
 ]
--- theapplicationswhiz/views.py
+++ theapplicationswhiz/views.py
@@ -1,6 +1,14 @@
+from typing import List
 from django.shortcuts import render
+#allows to list a queryset and then bring them back to website, queryset brings back one record
+from django.views.generic import ListView, DetailView
+from theapplicationswhiz.models import Application
 
-# Create your views here.
+# Create your views here
 
-def home(request):
-    return render(request, 'home.html', {})
+
+
+# this is an example of a class view 
+class HomeView(ListView):
+    model = Application
+    template_name = 'home.html'
